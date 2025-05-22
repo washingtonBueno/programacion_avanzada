0248a8d1a6d8ed2fa1c8fb78b6fe8bfb98f0285a
washingtonBueno <waschin8@gmail.com>
2025-05-21 22:08:08 -0300

    añarrndt
    dgtbdt

---
+++ PruebaVariables.java
@@ -0,0 +1,90 @@
+//public class PruebaVariables  {   
+
+ //1. Declara una variable int llamada edad y asígnale el valor 17. Luego, imprímela por 
+//consola.
+
+    //public static void main(String[] args) {
+        //int edad = 17;
+        //System.out.println(edad);
+
+ //2. Declara una variable double llamada precioProducto con el valor 45.99.
+
+      //  public static void main(String[] args) {
+        //double precioProducto = 45.99;
+       // System.out.println(precioProducto);
+    
+    // 3. Crea una variable boolean llamada tieneDescuento y asígnale el valor true.
+
+ //public static void main(String[] args) {
+   //     boolean tieneDescuento = true;
+     //   System.out.println(tieneDescuento);{
+
+ //4. Declara una variable char que contenga la letra inicial de tu nombre.
+
+     //char inicial = 'W';
+   
+//5. Declara una variable int llamada puntos, asígnale 10, luego cambia su valor a 20 y 
+//vuelve a imprimirlo.
+
+   // public static void main(String[] args) {
+     //   int puntos = 10;
+       // System.out.println("Puntos: " + puntos);
+
+        //puntos = 20;
+        //System.out.println("Puntos: " + puntos);
+ 
+ //6. Declara una variable double llamada notaFinal. Luego crea una condición que imprima 
+//"Aprobado" si la nota es mayor o igual a 6, o "Desaprobado" si es menor.
+
+  //public static void main(String[] args) {
+    //    double notaFinal = 4.5;
+
+      //  if (notaFinal >= 6) {
+        //    System.out.println("Aprobado");
+        //} else {
+          //  System.out.println("Desaprobado");
+     
+ //7. Declara una variable double llamada temperatura sin asignarle valor. Luego asígnale 
+//36.6 e imprímela.
+
+   //  public static void main(String[] args) {
+     //   double temperatura; 
+       // temperatura = 36.6; 
+        //System.out.println(temperatura);
+    
+//8. Declara una variable int con un nombre inválido (por ejemplo, que empiece con un 
+//número). ¿Qué error te da el compilador?
+
+      //   public static void main(String[] args) {
+        //int 1numero = 5;
+        //System.out.println(1numero);
+ 
+// 9. Declara una variable que almacene si el usuario es mayor de edad (usa boolean) y 
+//muestra un mensaje condicional.
+
+ // public static void main(String[] args) {
+   //     boolean esMayorDeEdad = false; 
+
+     //   if (esMayorDeEdad) {
+       //     System.out.println("El usuario es mayor de edad.");
+        //} else {
+          //  System.out.println("El usuario es menor de edad.");
+        
+ //10. Declara tres variables del mismo tipo int en una sola línea: x, y, z, y asígnales valores
+ //diferentes
+
+// public static void main(String[] args) {
+  //      int x = 5, y = 10, z = 15;
+
+//}
+
+
+
+
+
+//parte 2
+
+//1:una varriable en java es un pedacito de memoria para almacenar un dato que nosotros le asignemos 
+//2:
+//3: la diferencia es que al declarar elegis el tipo de variable que queres utilizar dependiendo de lo que quieras hacer, texto o numeros enteros y despues le asignamos el valor que quieramos o necesitemos
+//4:pensamos que es que no se pueden asignar valores a variables incorrectas, por ejemplo asignarle un numero entero a un string que es para cadena de texto
